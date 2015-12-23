401e9a4b79a76a02cb06ea8129e900c95e4ae2de
Raymond Jackson <rjackson1989@temple.edu>
2015-12-22 19:24:58 -0500

    added double carousel

--- js/script.js
+++ js/script.js
@@ -5,4 +5,49 @@ $(document).ready(function(){
 		$(".active").removeClass("active");
 		$(this).addClass("active");
 	});
+
+	$('#lgCarousel').carousel({
+                interval: 5000
+        });
+ 
+        $('#carousel-text').html($('#slide-content-0').html());
+ 
+        //Handles the carousel thumbnails
+       $('[id^=carousel-selector-]').click( function(){
+            var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+            var id = parseInt(id);
+            $('#lgCarousel').carousel(id);
+        });
+ 
+ 
+        // When the carousel slides, auto update the text
+        $('#lgCarousel').on('slid.bs.carousel', function (e) {
+                 var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        }); 
+	$('#miniCarousel').carousel({
+		interval: 40000
+	});
+
+	$('#miniCarousel .item').each(function(){
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+
+	for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+
+    next.children(':first-child').clone().appendTo($(this));
+	}
+	$('[id^=carousel-selector-]').click( function(){
+            var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+            var id = parseInt(id);
+            $('#lgCarousel').carousel(id);
+        });
+	});
 });
